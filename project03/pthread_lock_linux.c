68f34e4b222ff16e53886bd34a88551cc32fa20e
gudtjd82 <gudtjd82@naver.com>
2024-06-03 18:32:26 +0900

    project04 setting

--- project03/pthread_lock_linux.c
+++ project03/pthread_lock_linux.c
@@ -4,27 +4,41 @@
 int shared_resource = 0;
 
 #define NUM_ITERS 1000
-#define NUM_THREADS 10
+#define NUM_THREADS 100
 
-void lock();
-void unlock();
+volatile int flag[NUM_THREADS];
+volatile int turn[NUM_THREADS-1];
 
-void lock()
+void lock(int);
+void unlock(int);
+
+
+void lock(int tid)
 {
+    for (int i = 0; i < NUM_THREADS - 1; ++i) 
+    {
+        flag[tid] = i + 1;
+        turn[i] = tid;
+        for (int other = 0; other < NUM_THREADS; ++other) {
+            if (other == tid) continue;
+            while (!(flag[other] < flag[tid] || turn[i] != tid));
+        }
+    }
 }
 
-void unlock()
+void unlock(int tid)
 {
+    flag[tid] = 0;
 }
 
 void* thread_func(void* arg) {
     int tid = *(int*)arg;
     
-    // lock();
+    lock(tid);
     
         for(int i = 0; i < NUM_ITERS; i++)    shared_resource++;
     
-    // unlock();
+    unlock(tid);
     
     pthread_exit(NULL);
 }
@@ -33,7 +47,7 @@ int main() {
     int n = NUM_THREADS;
     pthread_t threads[n];
     int tids[n];
-    
+
     for (int i = 0; i < n; i++) {
         tids[i] = i;
         pthread_create(&threads[i], NULL, thread_func, &tids[i]);
